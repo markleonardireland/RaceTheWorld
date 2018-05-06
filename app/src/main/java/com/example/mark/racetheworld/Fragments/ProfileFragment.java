740d56c6c83c7a78d36057c7c3e86eb5787f93ac
Mark Leonard <markleonardireland@gmail.com>
2018-05-06 11:48:26 +0100

    Added util for downloading images form URL

--- app/src/main/java/com/example/mark/racetheworld/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/mark/racetheworld/Fragments/ProfileFragment.java
@@ -7,12 +7,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
 import com.example.mark.racetheworld.R;
+import com.example.mark.racetheworld.Utilities.ImageHelper;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        ImageView image = view.findViewById((R.id.profile_image));
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        ImageHelper helper = new ImageHelper();
+
+        helper.setImageFromUrl(image, user.getPhotoUrl().toString());
+
+        return view;
     }
 }
