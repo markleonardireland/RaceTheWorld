5d6baf99ddfbe3f7b1484c773d7bd597a340feac
Mark Leonard <markleonardireland@gmail.com>
2018-05-06 11:02:53 +0100

    hooked up logout to menu button

--- app/src/main/java/com/example/mark/racetheworld/Activities/GoogleLogOut.java
+++ app/src/main/java/com/example/mark/racetheworld/Activities/GoogleLogOut.java
@@ -51,7 +51,6 @@ public class GoogleLogOut extends AppCompatActivity {
                 }
             }
         };
-
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/mark/racetheworld/Activities/Menu.java
+++ app/src/main/java/com/example/mark/racetheworld/Activities/Menu.java
@@ -1,5 +1,6 @@
 package com.example.mark.racetheworld.Activities;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -9,18 +10,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.mark.racetheworld.Fragments.ChallengeFragment;
 import com.example.mark.racetheworld.Fragments.ProfileFragment;
 import com.example.mark.racetheworld.R;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Menu extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
+    private FirebaseAuth mAuth;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        Toast.makeText(Menu.this, "Something Went Wrong", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        mGoogleApiClient.connect();
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -51,6 +79,18 @@ public class Menu extends AppCompatActivity implements NavigationView.OnNavigati
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new ChallengeFragment()).commit();
                 break;
+            case R.id.nav_logout:
+                mAuth = FirebaseAuth.getInstance();
+                mAuth.signOut();
+                // Google sign out
+                Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                        new ResultCallback<Status>() {
+                            @Override
+                            public void onResult(@NonNull Status status) {
+                                Intent intent = new Intent(Menu.this, GoogleSignIn.class);
+                                startActivity(intent);
+                            }
+                        });
         }
 
         return true;
