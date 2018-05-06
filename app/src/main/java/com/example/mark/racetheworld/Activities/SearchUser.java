5312e588b6ae2f634f49d302beea9724f53616d6
Mark Leonard <markleonardireland@gmail.com>
2018-05-06 12:10:21 +0100

    updating search

--- app/src/main/java/com/example/mark/racetheworld/Activities/SearchUser.java
+++ app/src/main/java/com/example/mark/racetheworld/Activities/SearchUser.java
@@ -1,12 +1,22 @@
 package com.example.mark.racetheworld.Activities;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.widget.Button;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.mark.racetheworld.FireBase.User;
 import com.example.mark.racetheworld.R;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class SearchUser extends AppCompatActivity {
 
@@ -15,14 +25,92 @@ public class SearchUser extends AppCompatActivity {
 
     private RecyclerView mResultList;
 
+    private DatabaseReference mUserDatabase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_user);
 
+        mUserDatabase = FirebaseDatabase.getInstance().getReference("Users");
+
+
         mSearchField = (EditText) findViewById(R.id.search_field);
         mSearchBtn = (Button) findViewById(R.id.search_btn);
 
         mResultList = (RecyclerView) findViewById(R.id.result_list);
+        mResultList.setHasFixedSize(true);
+        mResultList.setLayoutManager(new LinearLayoutManager(this));
+
+        mSearchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String searchText = mSearchField.getText().toString();
+
+                firebaseUserSearch(searchText);
+
+            }
+        });
+
     }
+
+    private void firebaseUserSearch(String searchText) {
+
+        Toast.makeText(SearchUser.this, "Started Search", Toast.LENGTH_LONG).show();
+
+        Query firebaseSearchQuery = mUserDatabase.orderByChild("name").startAt(searchText).endAt(searchText + "\uf8ff");
+
+        FirebaseRecyclerAdapter<User, UsersViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<User, UsersViewHolder>(
+
+                User.class,
+                R.layout.list_layout,
+                UsersViewHolder.class,
+                firebaseSearchQuery
+
+        ) {
+            @Override
+            protected void populateViewHolder(UsersViewHolder viewHolder, User model, int position) {
+
+
+                viewHolder.setDetails(getApplicationContext(), model.name, model.racesWon);
+
+            }
+        };
+
+        mResultList.setAdapter(firebaseRecyclerAdapter);
+
+    }
+
+
+    // View Holder Class
+
+    public static class UsersViewHolder extends RecyclerView.ViewHolder {
+
+        View mView;
+
+        public UsersViewHolder(View itemView) {
+            super(itemView);
+
+            mView = itemView;
+
+        }
+
+        public void setDetails(Context ctx, String userName, int racesWon){
+
+            TextView user_name = (TextView) mView.findViewById(R.id.name_text);
+            TextView races_won = (TextView) mView.findViewById(R.id.races_won);
+
+
+
+            user_name.setText(userName);
+            races_won.setText(racesWon);
+
+        }
+
+
+
+
+    }
+
 }
